083bc6325e57e2a41d4a64da13b685eb022e8f11
Hitarth Rajani <hitarth_rajani@softude.com>
2026-06-24 12:09:07 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref:"user"
+    },
+    channel:{
+        type: Schema.Types.ObjectId, // one to whom subscriber is subscribing
+        ref:"user"
+    }
+    
+}, { timestamps:true})
+
+export const Subscription = mongoose.model("Subscription",subscriptionSchema)
